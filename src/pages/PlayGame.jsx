49fc1c6c53e895a61f9445ec366101e57d62fbf7
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2024-12-02 01:31:33 +0530

    Added hangman

--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -1,16 +1,46 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
+import Maskedtext from "../Components/MaskedText/MaskedText";
+import LetterButtons from "../Components/LettersButtons/LetterButtons";
+import { useState } from "react";
+import HangMan from "../Components/Hangman/HangMan";
 
 function PlayGame() {
+ 
+  const {state} = useLocation()
+  console.log(state.wordSelected )
 
+  const [steps , setSteps] = useState(0);
 
+  const [guessedLetters , setGuessedLetters] = useState([]);
+
+  function HandleLetterClick(letters){
+    if(state.wordSelected.toUpperCase().includes(letters)){
+       console.log("correct")
+    } else {
+      console.log("wrong");
+      setSteps(steps+1);
+    }
+     setGuessedLetters([...guessedLetters ,letters])
+  }
  return (
      <>
        <h1>Play Game</h1>
+
+       <Maskedtext text={state.wordSelected} guessedLetters={guessedLetters}/>
+       <div>
+   
+       <LetterButtons text={state.wordSelected} guessedLetters={guessedLetters} onLetterClick={HandleLetterClick} />
+
+       </div>
+      <div>
+        <HangMan steps={steps} />
+      </div>
+
        <Link to='/start' className='text-green-300'>StartGame</Link>
      </>
 
  )
 
 }
-
-export default PlayGame
+ 
+export default PlayGame;
